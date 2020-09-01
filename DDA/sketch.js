ce7da547849cef2d87d89f391651b424e9554ca5
bhupe <bhupeshjoshi293@gmail.com>
2020-09-01 23:28:42 +0545

    graphicslab2

---
+++ DDA/sketch.js
@@ -0,0 +1,40 @@
+function setup() {
+    createCanvas(500, 500);
+}
+
+function draw() {
+    background(230);
+
+    let x1 = 60, y1 = 60, x2 = 210, y2 = 100;
+
+    const m = (y2 - y1) / (x2 - x1)
+
+    fill(0, 0, 0)
+
+    if (Math.abs(m) <= 1) {
+        let i;
+        beginShape();
+        vertex(x1, y1);
+        for (i = x1; i < x2; i++) {
+            if (x1 < x2) {
+                x1 = x1 + 1;
+                y1 = y1 + m;
+                vertex(x1, y1)
+            }
+        }
+        vertex(x2, y2);
+        endShape();
+    } else if (Math.abs(m) > 1) {
+        let i;
+        beginShape();
+        vertex(x1, y1);
+        for (i = y1; i < y2; i++) {
+            if (y1 < y2) {
+                y1 = y1 + 1;
+                x1 = x1 + 1 / m;
+            }
+        }
+        vertex(x2, y2);
+        endShape();
+    }
+}
